92d3f34eb743851666ed2daf6562e767e9d19ef0
MiniThought17 <felixomwenga441@gmail.com>
2023-03-24 12:38:12 +0300

    few changes

--- bankdapp/src/components/ContractsServices/services.js
+++ bankdapp/src/components/ContractsServices/services.js
@@ -9,7 +9,7 @@ export function getAccountsContract(){
     //connect to the smart contract
     const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/")
     const address = process.env.REACT_APP_ACCOUNTS_ADDRESS
-    const accounts = new ethers.Contract("0x809d550fca64d94Bd9F66E60752A544199cfAC3D", AccountsABI, provider)
+    const accounts = new ethers.Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", AccountsABI, provider)
 
     //signer
     const accountsSigner = provider.getSigner()
@@ -20,7 +20,7 @@ export function getAccountsContract(){
 export function getLoansContract(){
     const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/")
     const address = process.env.REACT_APP_LOANS_ADDRESS;
-    const loans = new ethers.Contract("0xc351628EB244ec633d5f21fBD6621e1a683B1181", LoansABI, provider);
+    const loans = new ethers.Contract("0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9", LoansABI, provider);
 
     //signer
     const loansSigner = provider.getSigner()
